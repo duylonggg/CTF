608ac272989703806773bd844d60ac480356da01
duylonggg <haduylong@gmail.com>
2025-05-17 20:06:55 +0700

    CTF

---
+++ Dreamhack/RE/rev-basic-8/solve.py
@@ -0,0 +1,17 @@
+byte_140003000 = [
+    0xAC, 0xF3, 0x0C, 0x25, 0xA3, 0x10, 0xB7, 0x25, 0x16, 0xC6,
+    0xB7, 0xBC, 0x07, 0x25, 0x02, 0xD5, 0xC6, 0x11, 0x07, 0xC5
+]
+
+result = []
+
+for val in byte_140003000:
+    found = False
+    for x in range(256):
+        if (-5 * x) & 0xFF == val:
+            result.append(x)
+            found = True
+            break
+
+print("Recovered ASCII:", bytes(result).decode(errors='replace'))
+
